067879181d90f19f87aecdec526ef91192eae697
SimonNes112 <164831@student.horizoncollege.nl>
2022-09-19 16:43:16 +0200

    Co-authored-by: Wessel-Stam <Wessel-Stam@users.noreply.github.com>

---
+++ db/migrate/20220919114814_create_tasks.rb
@@ -0,0 +1,12 @@
+class CreateTasks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :tasks do |t|
+      t.integer :user_id
+      t.date :date
+      t.string :task
+      t.boolean :status
+
+      t.timestamps
+    end
+  end
+end
