9bdc2ad0514fb66a391b838ecf70e1733cf8e5ff
SimonNes112 <164831@student.horizoncollege.nl>
2022-09-23 10:59:52 +0200

    Co-authored-by: Wessel-Stam <Wessel-Stam@users.noreply.github.com>

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,44 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
-import "controllers"
 
+import "./controllers";
 
-//require("packs/task")
-
-window.onload = function () {
-    bootlint.showLintReportForCurrentDocument([], {
-        hasProblems: false,
-        problemFree: false
-    });
-
-    $('[data-toggle="tooltip"]').tooltip();
-
-    function formatDate(date) {
-        return (
-            date.getDate() +
-            "/" +
-            (date.getMonth() + 1) +
-            "/" +
-            date.getFullYear()
-        );
-    }
-
-    var currentDate = formatDate(new Date());
-
-    $(".due-date-button").datepicker({
-        format: "dd/mm/yyyy",
-        autoclose: true,
-        todayHighlight: true,
-        startDate: currentDate,
-        orientation: "bottom right"
-    });
-
-    $(".due-date-button").on("click", function (event) {
-        $(".due-date-button")
-            .datepicker("show")
-            .on("changeDate", function (dateChangeEvent) {
-                $(".due-date-button").datepicker("hide");
-                $(".due-date-label").text(formatDate(dateChangeEvent.date));
-            });
-    });
-};
+import "./packs/task";
